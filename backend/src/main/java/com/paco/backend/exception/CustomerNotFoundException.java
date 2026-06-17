64b3d414d15bb03d8da0ebb26ddd96e506310031
Vikus7 <victor.rodriguez01@epn.edu.ec>
2026-06-16 23:38:46 -0500

    Backend: Add new exception management

--- backend/src/main/java/com/paco/backend/exception/CustomerNotFoundException.java
+++ backend/src/main/java/com/paco/backend/exception/CustomerNotFoundException.java
@@ -1,8 +1,7 @@
 package com.paco.backend.exception;
 
-/**
- * Se lanza cuando no se encuentra un cliente por su identificador.
- */
+// Se lanza cuando no se encuentra un cliente por su identificador.
+
 public class CustomerNotFoundException extends RuntimeException {
 
     public CustomerNotFoundException(String message) {
